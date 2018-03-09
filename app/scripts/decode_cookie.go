fa4f2cdb1c0d74a01f7dbd341930a0974ffd76ea
golangci <dev@golangci.com>
2018-03-09 17:14:00 +0300

    fetch 300 repos instead of 90

--- app/scripts/decode_cookie.go
+++ app/scripts/decode_cookie.go
@@ -13,20 +13,25 @@ func main() {
 	encode := flag.Bool("encode", true, "Encode/Decode")
 	flag.Parse()
 
-	codecs := securecookie.CodecsFromPairs([]byte(os.Getenv("SESSION_SECRET")))
+	sessSecret := os.Getenv("SESSION_SECRET")
+	if sessSecret == "" {
+		panic("SESSION_SECRET isn't set")
+	}
+
+	codecs := securecookie.CodecsFromPairs([]byte(sessSecret))
 	if *encode {
 		encodedValue, err := securecookie.EncodeMulti("s", *value, codecs...)
 		if err != nil {
-			panic(err)
+			log.Fatalf("Can't encode: %s", err)
 		}
 
-		log.Printf("encoded: %q", encodedValue)
+		log.Printf("Encoded: %q", encodedValue)
 	} else {
 		var decodedValue string
 		if err := securecookie.DecodeMulti("s", *value, &decodedValue, codecs...); err != nil {
-			panic(err)
+			log.Fatalf("Can't decode %q: %s", *value, err)
 		}
 
-		log.Printf("decoded: %q", decodedValue)
+		log.Printf("Decoded: %q", decodedValue)
 	}
 }
