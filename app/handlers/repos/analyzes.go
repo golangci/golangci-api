25a91f3d4ce15d35e2910eecfff0245131040072
golangci <dev@golangci.com>
2018-04-13 21:43:08 +0300

    get analysis status API

--- app/handlers/repos/analyzes.go
+++ app/handlers/repos/analyzes.go
@@ -2,21 +2,49 @@ package repos
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
 
 	"github.com/golangci/golangci-api/app/handlers"
 	"github.com/golangci/golangci-api/app/internal/db"
 	"github.com/golangci/golangci-api/app/models"
 	"github.com/golangci/golib/server/context"
-	"github.com/golangci/golib/server/handlers/helpers"
 	"github.com/golangci/golib/server/handlers/herrors"
 )
 
-type statusUpdatePayload struct {
+type statusPayload struct {
 	Status string
 }
 
+func handleAnalysisStatus(ctx context.C) error {
+	switch ctx.R.Method {
+	case http.MethodGet:
+		return getAnalysisStatus(ctx)
+	case http.MethodPut:
+		return updateAnalysisStatus(ctx)
+	default:
+		return fmt.Errorf("not allowed method")
+	}
+}
+
+func getAnalysisStatus(ctx context.C) error {
+	analysisGUID := ctx.URLVar("analysisID")
+	var analysis models.GithubAnalysis
+	err := models.NewGithubAnalysisQuerySet(db.Get(&ctx)).
+		GithubDeliveryGUIDEq(analysisGUID).
+		One(&analysis)
+	if err != nil {
+		return herrors.New(err, "can't get github analysis with guid %s", analysisGUID)
+	}
+
+	ctx.ReturnJSON(statusPayload{
+		Status: analysis.Status,
+	})
+	return nil
+}
+
 func updateAnalysisStatus(ctx context.C) error {
-	var payload statusUpdatePayload
+	var payload statusPayload
 	if err := json.NewDecoder(ctx.R.Body).Decode(&payload); err != nil {
 		return herrors.New400Errorf("invalid payload json: %s", err)
 	}
@@ -41,5 +69,5 @@ func updateAnalysisStatus(ctx context.C) error {
 }
 
 func init() {
-	handlers.Register("/v1/repos/{owner}/{name}/analyzes/{analysisID}/status", helpers.OnlyPUT(updateAnalysisStatus))
+	handlers.Register("/v1/repos/{owner}/{name}/analyzes/{analysisID}/status", handleAnalysisStatus)
 }
