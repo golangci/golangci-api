ffef5bce5070ffce2b72d88feadac4c4e9b2a5ce
Eric Wohltman <eric.wohltman@gmail.com>
2019-12-30 15:02:15 -0500

    Add support for golangci-lint v1.22.x

--- pkg/goenvbuild/preparer.go
+++ pkg/goenvbuild/preparer.go
@@ -39,10 +39,11 @@ var availableGolangciLintVersions = map[int]map[int][]int{
 		19: {0, 1},
 		20: {0, 1},
 		21: {0},
+		22: {0, 1, 2},
 	},
 }
 
-const defaultGolangciLintVersion = "1.21.x"
+const defaultGolangciLintVersion = "1.22.x"
 
 type Preparer struct {
 	cfg config.Config
