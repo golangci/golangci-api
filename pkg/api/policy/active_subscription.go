fd38bda82371240877ccfb34ebd197f817910c16
Denis Isaev <denis@golangci.com>
2020-03-14 12:39:50 +0300

    add logging

--- pkg/api/policy/active_subscription.go
+++ pkg/api/policy/active_subscription.go
@@ -56,6 +56,7 @@ func (s ActiveSubscription) CheckForProviderRepo(p provider.Provider, pr *provid
 		return nil
 	}
 
+	s.log.Infof("Check active subscription by config")
 	_, _, err := s.getActiveSub(p, pr)
 	return err
 }
